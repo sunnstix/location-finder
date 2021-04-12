3d1a0ef5954d0d019bce8da60291d9d75bd7db7c
Skylor Grim <grims@umich.edu>
2021-04-11 22:07:39 -0400

    finished with preprocessing and converting raw tweets from csv collection to a JSON file

--- csv-to-json.py
+++ csv-to-json.py
@@ -1,7 +1,11 @@
 import os
 import sys
-
+import csv  # https://realpython.com/python-csv/
+import json # https://stackabuse.com/reading-and-writing-json-to-a-file-in-python/
+import re # to remove non-alphanumerics from strings
 # USAGE: python3 ./csv-to-json.py <valid csv file>
+# for us: python3 ./csv-to-json.py twitter-data.csv
+
 # Tasks:
 # 1. takes as input a .csv file and outputs a .json
 #    valid csv file has the following columns: tweet number, description, text, location
@@ -19,6 +23,68 @@ import sys
 #                   "vmi" --> invalid
 #                   "88.925" --> invalid
 
+# TIP: you may need to format your JSON file after running for clarity.
+#       ON VSCode for Mac: CMD + SHIFT + P, then type "Format Document", then enter
+
+# globals
+states = ['alabama', 'alaska', 'arizona',
+          'arkansas', 'california', 'colorado',
+          'conneticut', 'delaware', 'florida',
+          'georgia', 'hiwaii', 'idaho',
+          'illinois', 'indiana', 'iowa',
+          'kansas', 'kentucky', 'louisiana',
+          'maine', 'maryland', 'massachusets',
+          'michigan', 'minnesota', 'mississippi',
+          'missouri', 'montana', 'nebraska',
+          'nevada', 'new hampshire', 'new jersey',
+          'new mexico', 'new york', 'north carolina',
+          'north dakota', 'ohio', 'oklahoma',
+          'oregon', 'pennsylvania', 'rhode island',
+          'south carolina', 'south dakota', 'tennesse',
+          'texas', 'utah', 'vermont',
+          'virginia', 'washington', 'west virginia',
+          'wisconsin', 'wyoming']
+stateInitials = {
+    'al': states[0], 'ak': states[1], 'az': states[2],
+    'ar': states[3], 'ca': states[4], 'co': states[5],
+    'ct': states[6], 'de': states[7], 'fl': states[8],
+    'ga': states[9], 'hi': states[10], 'id': states[11],
+    'il': states[12], 'in': states[13], 'ia': states[14],
+    'ks': states[15], 'ky': states[16], 'la': states[17],
+    'me': states[18], 'md': states[19], 'ma': states[20],
+    'mi': states[21], 'mn': states[22], 'ms': states[23],
+    'mo': states[24], 'mt': states[25], 'ne': states[26],
+    'nv': states[27], 'nh': states[28], 'nj': states[29],
+    'nm': states[30], 'ny': states[31], 'nc': states[32],
+    'nd': states[33], 'oh': states[34], 'ok': states[35],
+    'or': states[36], 'pa': states[37], 'ri': states[38],
+    'sc': states[39], 'sd': states[40], 'tn': states[41],
+    'tx': states[42], 'ut': states[43], 'vt': states[44],
+    'va': states[45], 'wa': states[46], 'wv': states[47],
+    'wi': states[48], 'wy': states[49]
+}
+
+def validateLocation(location: str) -> str:
+    """ensures tweet has valid location. 
+    if true, return long form of state name
+    if not, return string "NULL" """
+    # check location
+    validLocation = "NULL"  # init to "NULL" incase no valid location is found
+    locationTokens = location.split()
+    for token in locationTokens:
+        # token = re.compile("[^a-zA-Z]") # removes all non-alpha chars
+        token = str(token)
+        # print("token = ", token)
+        if token in stateInitials:
+            # print("--found valid: ", stateInitials[token])
+            validLocation = stateInitials[token]
+            break
+        elif token in states:
+            # print("--found valid: ", token)
+            validLocation = token
+            break
+    return validLocation
+
 def main():
     """MAIN"""
     # check args
@@ -30,11 +96,53 @@ def main():
         print("Path: ", sys.argv[1], ", does not exist\nExiting now")
         exit(1)
     
-    # file management
-    curDir = os.getcwd()
-    outputFile = os.path.join(curDir, "data.json")  # needs to open still
-    inputFile = os.open(sys.argv[1])
+    # output file management
+    outputFileLocation = "data.json"
+    if os.path.exists(outputFileLocation):
+        os.remove(outputFileLocation)
+    outputFile = open(outputFileLocation, "x")
+    
+    # vars for csv
+    number = -1
+    desc = "empty"
+    text = "empty"
+    location = "empty"
+    refinedLocation = "empty" 
+    lineCount = 0
 
+    # vars for output
+    data = []
+    # tempTweet = {"number": number, "description": desc, "text": text, "location": refinedLocation}
+
+    # process csv
+    with open ("twitter-data.csv", encoding='utf-8', errors='ignore') as csv_file:
+        csv_data = csv.reader(csv_file, delimiter=",")
+        # -- number , description, text, location --
+        for row in csv_data:
+            # first line is headers and we should ignore this.
+            if lineCount != 0:
+                # print("line number: ", lineCount)
+                number = int(row[0])
+                desc = row[1].lower()
+                text = row[2].lower()
+                location = row[3].lower()
+
+                refinedLocation = validateLocation(location)
+
+                # we only want to keep VALID location tweets
+                if refinedLocation != "NULL":
+                    
+                    # append the new tweet
+                    data.append({"number": number, "description": desc, "text": text, "location": refinedLocation})
+
+            lineCount += 1
+            
+    # write data to output file
+    json.dump(data,outputFile)
+
+    # close files
+    csv_file.close()
+    outputFile.close()
 
 if __name__ == "__main__":
     main()
