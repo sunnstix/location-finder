6318552640e7f908064755e2235c3ecdc94efa93
sanketn <sanketn@umich.edu>
2021-04-14 08:49:04 -0400

    Fixed Dataloader and Removed old pycaches

--- dataloader.py
+++ dataloader.py
@@ -1,11 +1,11 @@
 import torch
-from torch._C import dtype
-from torchtext import data
+from torchtext.legacy import data
+from torchtext.legacy.data.field import Field, LabelField, Pipeline
 from preprocessing import PreProcess
 import random
 from utils import *
-
 import torch.optim
+
 
 CSTM_PRPRCSS = False #flag for custom preprocessing
 
@@ -13,44 +13,35 @@ MYSEED = 420 #seed with time later but using static seed for now
 
 #cuda check
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+TOKENIZER = PreProcess().tweet_preprocessor
 
 torch.manual_seed(MYSEED)
 torch.backends.cudnn.deterministic = True
 
-if not CSTM_PRPRCSS:
-    TXT = data.Field(tokenize='spacy',batch_first=True,include_lengths=True)
-else:
-    TXT = data.Field(preprocessing=PreProcess.preprocessFunction1,batch_first=True,include_lengths=True)
-LBL = data.LabelField(dtype = torch.int32,batch_first=True)
-
-def loaderDebug():
-    #No. of unique tokens in text
-    print("Size of TEXT vocabulary:",len(TXT.vocab))
-
-    #No. of unique tokens in label
-    print("Size of LABEL vocabulary:",len(LBL.vocab))
-
-    #Commonly used words
-    print(TXT.vocab.freqs.most_common(10))  
-
-    #Word dictionary
-    print(TXT.vocab.stoi)
+class TwitterData:
+    def __init__(self):
+        self.txtField = Field(tokenize=TOKENIZER,batch_first=True,include_lengths=True)
+        self.lblField = LabelField(preprocessing = Pipeline(lambda x: int(x)),batch_first = True, dtype = torch.float)
+        self.myData = data.TabularDataset(path = config('csv_file'), format = 'csv', fields = [('text',self.txtField),('label',self.lblField)], skip_header = True)
 
 
-def load(debug):
-    #load data in
-    jsonFields = {'description':('desc',None),'text':('text',TXT),'location':('label',LBL)}
-    myData = data.TabularDataset(path = config('json_file'), format = 'json', fields = jsonFields, skip_header = False)
+        self.trainData,self.validData = self.myData.split(split_ratio = 0.7, random_state = random.seed(MYSEED))
 
-    #test print
-    if debug: print(vars(myData.examples[0]))
-    trainData,validData = myData.split(split_ratio = 0.7, random_state = random.seed(MYSEED))
+        #utilize glove embeddings
+        self.txtField.build_vocab(self.trainData,vectors = 'glove.6B.100d')
+        self.lblField.build_vocab(self.trainData)
 
-    #utilize glove embeddings
-    TXT.build_vocab(trainData,vectors = 'glove.6B.100d', min_freq = 5)
-    LBL.build_vocab(trainData)
-    if debug: loaderDebug()
+    def load(self,debug=False):
+        #test print
+        if debug: print(vars(self.myData.examples[0]),vars(self.myData.examples[1]))
+        if debug: 
+            print(vars(self.myData.examples[0]),vars(self.myData.examples[1]))
+            print(self.txtField.vocab.vectors)
+            print(self.lblField.vocab.vectors)
 
-    #iterator generator
-    return data.BucketIterator.splits((trainData, validData), batch_size = config('batch_size'), 
-                                    sort_key = lambda s: len(s.text), sort_within_batch = True, device = DEVICE)
+        #iterator generator
+        return data.BucketIterator.splits((self.trainData, self.validData), batch_size = config('batch_size'), 
+                                        sort_key = lambda s: len(s.text), sort_within_batch = True, device = DEVICE)
+
+if __name__ == '__main__':
+    TwitterData().load(True)
