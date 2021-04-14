a3e5bd28bdd49ec82c45df7c8cfd3e822887d4d9
AlPal <arudelic@umich.edu>
2021-04-14 13:14:07 -0400

    Removed unneccessary package and added additional option

--- preprocessing.py
+++ preprocessing.py
@@ -1,18 +1,16 @@
 from tweet_preprocessor import preprocessor
-import nltk
-
-
 
 class PreProcess():
     def __init__(self):
         return 
 
     #Preprocesses tweets using tweet-preprocessor libarary!
-    #Experiment with options by changing the mode parameter to a value 1-5:
+    #Experiment with options by changing the mode parameter to a value 1-4:
     #Possible options:
     #1 - clean hashtags, mentions, and numbers
     #2 - clean mentions and numbers
     #3 - clean hashtags and numbers
+    #4 - clean numbers
     def tweet_preprocessor(self, tweet_str, mode=1):
 
         #Set to lowercase
@@ -38,6 +36,12 @@ class PreProcess():
             preprocessor.OPT.EMOJI,
             preprocessor.OPT.SMILEY,
             preprocessor.OPT.NUMBER)
+        elif mode == 4:
+            preprocessor.set_options(preprocessor.OPT.RESERVED,
+            preprocessor.OPT.EMOJI,
+            preprocessor.OPT.SMILEY,
+            preprocessor.OPT.NUMBER)
+
 
         #Clean tweet
         tweet_str = preprocessor.clean(tweet_str)
