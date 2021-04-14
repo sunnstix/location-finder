b9500dedf843f68e6ef6d80b759769abf772fad4
AlPal <arudelic@umich.edu>
2021-04-13 23:37:44 -0400

    Added preprocessing function

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,49 @@
+from tweet_preprocessor import preprocessor
+import nltk
+
+
+
 class PreProcess():
-    def __init__():
+    def __init__(self):
         return 
-    def preprocessFunction1(str):
-        return str
-    def preprocessFunction2(str):
-        return str
+
+    #Preprocesses tweets using tweet-preprocessor libarary!
+    #Experiment with options by changing the mode parameter to a value 1-5:
+    #Possible options:
+    #1 - clean hashtags, mentions, and numbers
+    #2 - clean mentions and numbers
+    #3 - clean hashtags and numbers
+    def tweet_preprocessor(self, tweet_str, mode):
+
+        #Set to lowercase
+        tweet_str = tweet_str.lower()
+
+        #Set cleaning options
+        if mode == 1:
+            preprocessor.set_options(preprocessor.OPT.HASHTAG,
+            preprocessor.OPT.MENTION,
+            preprocessor.OPT.RESERVED,
+            preprocessor.OPT.EMOJI,
+            preprocessor.OPT.SMILEY,
+            preprocessor.OPT.NUMBER)
+        elif mode == 2:
+            preprocessor.set_options(preprocessor.OPT.MENTION,
+            preprocessor.OPT.RESERVED,
+            preprocessor.OPT.EMOJI,
+            preprocessor.OPT.SMILEY,
+            preprocessor.OPT.NUMBER)
+        elif mode == 3:
+            preprocessor.set_options(preprocessor.OPT.HASHTAG,
+            preprocessor.OPT.RESERVED,
+            preprocessor.OPT.EMOJI,
+            preprocessor.OPT.SMILEY,
+            preprocessor.OPT.NUMBER)
+
+        #Clean tweet
+        tweet_str = preprocessor.clean(tweet_str)
+
+        return tweet_str
+
+test = PreProcess()
+
+print(test.tweet_preprocessor("@this is a #test lmao", 2))
